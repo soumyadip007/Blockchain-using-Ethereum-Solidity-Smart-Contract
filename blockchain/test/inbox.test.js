405cb3c3cc2aaa96a5c7af5566a36f759382e1cf
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-07 21:42:20 +0530

    Update inbox.test.js
    Passing contracts

--- blockchain/test/inbox.test.js
+++ blockchain/test/inbox.test.js
@@ -3,6 +3,24 @@ const ganache = require('ganache-cli');
 const Web3 = require('web3');
 const web3= new Web3(ganache.provider());
 
+beforeEach(()=>{
+
+	web3.eth.getAccounts()
+		.then(fetchAccounts=>{
+			console.log(fetchAccounts);
+		});
+
+});
+
+
+describe('Inbox',()=>{
+	
+	it('Deploys a contract',()=>{
+	
+	});
+
+});
+/*
 class Car{
 
 	park(){
@@ -38,3 +56,4 @@ describe('Car',()=>{
 	});
 
 });
+*/
