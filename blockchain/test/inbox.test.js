d5af718e976246ba11bc443f03037c28567b9539
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-07 20:52:52 +0530

    Test Passed Mocha Assert Testing

--- blockchain/test/inbox.test.js
+++ blockchain/test/inbox.test.js
@@ -1,5 +1,25 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-
 const web3= new Web3(ganache.provider());
+
+class Car{
+
+	park(){
+		return 'stopped';
+	}
+
+	drive(){
+		return 'room'
+	}
+}
+
+
+describe('Car',()=>{
+	
+	it('can park',()=>{
+		const car=new Car();
+		assert.equal(car.park(),'stopped');
+	});
+
+});
