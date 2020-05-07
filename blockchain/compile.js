1025724f5bf3047f7f91379c6085dd24c9611eee
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-07 22:16:15 +0530

    Updare

--- blockchain/compile.js
+++ blockchain/compile.js
@@ -1,10 +1,12 @@
-  
 const path = require('path');
 const fs = require('fs');
 const solc = require('solc');
+const{interface, bytecode}=require('../compile');
+
 
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
+
 
 //console.log(solc.compile(source,1));
 module.exports = solc.compile(source,1).contracts[':Inbox'];
