be0d3c7a439942f90f2043e425d9f14975c989d5
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-08 01:25:30 +0530

    Testing Change

--- blockchain/compile.js
+++ blockchain/compile.js
@@ -6,5 +6,5 @@ const solc = require('solc');
 const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
 const source = fs.readFileSync(inboxPath, 'utf8');
 
-//console.log(solc.compile(source,1));
+console.log(solc.compile(source,1));
 module.exports = solc.compile(source,1).contracts[':Inbox'];
