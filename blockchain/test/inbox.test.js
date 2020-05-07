4a465c83757e30fd97062fd54b96a28f16c82f07
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-07 23:04:24 +0530

    Inbox test.js

--- blockchain/test/inbox.test.js
+++ blockchain/test/inbox.test.js
@@ -1,23 +1,32 @@
 const assert = require('assert');
 const ganache = require('ganache-cli');
 const Web3 = require('web3');
-const web3= new Web3(ganache.provider());
+const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require('../compile');
 
 let accounts;
+let inbox;
 
-beforeEach(async()=>{
-	//list of all ac
-	accounts=await web3.eth.getAccounts();
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
 
-
+  // Use one of those accounts to deploy
+  // the contract
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ['Hi there!']
+    })
+    .send({ from: accounts[0], gas: '1000000' });
 });
 
 
 describe('Inbox',()=>{
 	
-	it('Deploys a contract',()=>{
-		console.log(accounts);
-	});
+	  it('deploys a contract', () => {
+  console.log(inbox);
+  });
 
 });
 
