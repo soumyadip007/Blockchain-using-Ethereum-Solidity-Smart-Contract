c475a19e4d22ae6465da387492b8a77f6a8b2f0e
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-10 01:43:54 +0530

    Update Lottery.sol

--- Lottery/contracts/Lottery.sol
+++ Lottery/contracts/Lottery.sol
@@ -1,42 +1,34 @@
 pragma solidity ^0.4.17;
 
 contract Lottery {
-    
     address public manager;
     address[] public players;
-    
-    function Lottery() public{
-        manager=msg.sender;
-        
+
+    function Lottery() public {
+        manager = msg.sender;
     }
 
-    function enter() public {
-        
-     //   require(msg.value>.01 ether );
-        
+    function enter() public payable {
+
         players.push(msg.sender);
     }
-    
-    function random() private view returns(uint256){
+
+    function random() private view returns (uint) {
         return uint(keccak256(block.difficulty, now, players));
     }
-    
-    function pickWinner() public restricted{
-  //      require(msg.sender==manager);
-        
+
+    function pickWinner() public restricted {
         uint index = random() % players.length;
-         players[index].transfer(this.balance);
-        players=new address[](0);
+        players[index].transfer(this.balance);
+        players = new address[](0);
     }
-    
-    modifier restricted(){
-        require(msg.sender==manager);  
+
+    modifier restricted() {
+        require(msg.sender == manager);
         _;
     }
-    
-    function getPlayers() public view returns(address[]){
+
+    function getPlayers() public view returns (address[]) {
         return players;
     }
-    
-    
 }
