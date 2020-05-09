1cb776c8964191d5c047c5bfd74658c9d73bf696
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-10 01:15:54 +0530

    Create Lottery.sol

---
+++ Lottery/contracts/Lottery.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.4.17;
+
+contract Lottery {
+    
+    address public manager;
+    address[] public players;
+    
+    function Lottery() public{
+        manager=msg.sender;
+        
+    }
+
+    function enter() public {
+        
+     //   require(msg.value>.01 ether );
+        
+        players.push(msg.sender);
+    }
+    
+    function random() private view returns(uint256){
+        return uint(keccak256(block.difficulty, now, players));
+    }
+    
+    function pickWinner() public restricted{
+  //      require(msg.sender==manager);
+        
+        uint index = random() % players.length;
+         players[index].transfer(this.balance);
+        players=new address[](0);
+    }
+    
+    modifier restricted(){
+        require(msg.sender==manager);  
+        _;
+    }
+    
+    function getPlayers() public view returns(address[]){
+        return players;
+    }
+    
+    
+}
