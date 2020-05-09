ce37336f7524b23f36cd69dde5026b9bb83c98de
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-10 01:43:56 +0530

    Update compile.js

--- Lottery/compile.js
+++ Lottery/compile.js
@@ -5,6 +5,6 @@ const solc = require('solc');
 
 const lotteryPath = path.resolve(__dirname, 'contracts', 'Lottery.sol');
 const source = fs.readFileSync(lotteryPath, 'utf8');
-
-console.log(solc.compile(source,1));
-module.exports = solc.compile(source,1).contracts[':Inbox'];
+//console.log(solc.compile(source,1));
+console.log('Compiling Lottery');
+module.exports = solc.compile(source, 1).contracts[':Lottery'];
