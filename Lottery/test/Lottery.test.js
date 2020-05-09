33c62f2edbcc43a6498b9cc7a93ffaeab3b7ecc0
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-10 01:15:49 +0530

    Create Lottery.test.js

---
+++ Lottery/test/Lottery.test.js
@@ -0,0 +1,33 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const web3 = new Web3(ganache.provider());
+
+const { interface, bytecode } = require('../compile');
+
+let lottery;
+let accounts;
+
+beforeEach(async () => {
+  
+  accounts = await web3.eth.getAccounts();
+
+  // Use one of those accounts to deploy
+  // the contract
+  lottery = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode })
+    .send({ from: accounts[0] , gas:'1000000'});
+});
+
+
+describe('Lottery Contract',()=>{
+	
+
+	  it('deploys a contract', () => {
+	  			console.log(lottery.options.address);
+	  			assert.ok(lottery.options.address);
+	  });
+
+});
+
