3e8a116680409bc700cb04edc582ee2e15bbad63
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-10 01:43:52 +0530

    Update deploy.js

--- Lottery/deploy.js
+++ Lottery/deploy.js
@@ -1,4 +1,4 @@
-const HDWalletProvider = require('truffle-hdwallet-provider');
+ const HDWalletProvider = require('truffle-hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
 
@@ -14,7 +14,7 @@ const deploy = async () => {
   console.log('Attempting to deploy from account', accounts[0]);
 
   const result = await new web3.eth.Contract(JSON.parse(interface))
-    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .deploy({ data: bytecode})
     .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
